4f403bd5941e0257bc749d3f42cf449377e27c41
Dan Morse <djmthedev@gmail.com>
2020-07-10 08:43:56 -0700

    initial

---
+++ CrossTalk/CrossTalk/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CrossTalk
+//
+//  Created by morse on 7/9/20.
+//  Copyright © 2020 morse. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
