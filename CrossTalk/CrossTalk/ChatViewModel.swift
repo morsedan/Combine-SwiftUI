1fe097cc7dc8beb725065d849e2a3b49c1b96480
Dan Morse <djmthedev@gmail.com>
2020-07-18 16:03:33 -0700

    added ChatViewModel

---
+++ CrossTalk/CrossTalk/ChatViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  ChatViewModel.swift
+//  CrossTalk
+//
+//  Created by morse on 7/9/20.
+//  Copyright © 2020 morse. All rights reserved.
+//
+
+import Foundation
+import MultipeerConnectivity
+import Combine
+import SwiftUI
+
+final class ChatViewModel: NSObject, ObservableObject {
+    
+    
+    
+}
